fbffd6fe03ecad7b43026153bbea76ed751aad78
larissaRF <larissa_lrf@hotmail.com>
2024-02-03 19:00:44 -0300

    audio

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const botaoPlayPause = document.getElementById('play-pause')
+const audioCapitulo = document.getElementById('audio-capitulo')
 const numeroCapitulos = 10
+
+function tocarFaixa() {
+    audioCapitulo.play()
+}
