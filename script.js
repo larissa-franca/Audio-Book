f971db5cde366bf3f78b1c5a71fa92adbe50007b
larissaRF <larissa_lrf@hotmail.com>
2024-02-03 19:15:22 -0300

    audio

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const botaoPlayPause = document.getElementById('play-pause')
 const audioCapitulo = document.getElementById('audio-capitulo')
 const numeroCapitulos = 10
+let taTocando = 0
 
 function tocarFaixa() {
     audioCapitulo.play()
 }
+
+function pausarFaixa() {
+    audioCapitulo.pause()
+}
+
+function tocarOuPausar() {
+    if (taTocando === 0) {
+        tocarFaixa()
+        taTocando = 1
+    } else {
+        pausarFaixa()
+        taTocando = 0
+    }
+}
+
+botaoPlayPause.addEventListener('click', tocarFaixa)
